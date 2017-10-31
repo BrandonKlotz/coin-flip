14a2649b9b135f078ad75a30f6de2338e259fc49
Brandon Klotz <brandonklotz@icloud.com>
2017-10-31 10:25:05 -0400

    Update functionality

--- js/script.js
+++ js/script.js
@@ -1,18 +1,35 @@
 (function() {
 
-$('button').on('click', function() { // Flip Button
+	let totalHeads = 0;
+	let totalTails = 0;
+	let totalCorrect = 0;
+ 	let totalWrong = 0;
 
+	updateDisplay();
+
+
+	$('.guess').on('keypress' , function(event) {
+		if (event.key === 'Enter') {
+			flipTheCoin();
+		}
+	});
+	$('#flip-button').on('click', function(){
+		flipTheCoin();
+	});
+
+function getHeadsOrTails() {
+	return Math.floor(Math.random() * 2);
+}
+
+function flipTheCoin() {
 	$('.coin-flip').css('display','block');
-
-	function getHeadsOrTails() {
-		return Math.floor(Math.random() * 2);
-	}
-
 	let flip = getHeadsOrTails();
 
 	if (flip > 0.5) {
+		totalHeads++;
 		var result = "heads";
 	} else {
+		totalTails++;
 		result = "tails";
 	}
 
@@ -27,19 +44,28 @@ $('button').on('click', function() { // Flip Button
 	$('#display-result').text(result);
 
 	if (guess === result) {
+		totalCorrect++;
 		$('#validate').text("Nice guess! You got it right.");
 		$('#validate').css('color', 'green');
 		$('#emoji').text('');
 		$('#emoji').text('😃');
 	} else {
+		totalWrong++;
 		$('#validate').text("Your guess was incorrect.");
 		$('#validate').css('color', 'red');
 		$('#emoji').text('');
 		$('#emoji').text('☹️');
 	}
 
-	$('.coin-flip').fadeOut(3000);
+	updateDisplay();
 
-});
+	}
+
+	function updateDisplay() {
+		$('.totalHeads').text(totalHeads);
+		$('.totalTails').text(totalTails);
+		$('.totalCorrect').text(totalCorrect);
+		$('.totalWrong').text(totalWrong);
+	}
 
 })();
